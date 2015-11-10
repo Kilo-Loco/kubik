2ff12af51208a9e281f05b6b8c6c5ced1861c9b2
Kyle Lee <kiloloco@me.com>
2015-11-10 13:12:58 -0800

    Implemented timer, penalties, and death animation

--- Kubik/ViewController.swift
+++ Kubik/ViewController.swift
@@ -10,25 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var monsterImg: KubikImg!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    @IBOutlet var penalty1Img: UIImageView!
+    @IBOutlet var penalty2Img: UIImageView!
+    @IBOutlet var penalty3Img: UIImageView!
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTY = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var imgArray = [UIImage]()
-        for var x = 1; x <= 4; x++ {
-            let img = UIImage(named: "idle\(x).png")
-            imgArray.append(img!)
-        }
+        self.foodImg.dropTarget = self.monsterImg
+        self.heartImg.dropTarget = self.monsterImg
         
-        monsterImg.animationImages = imgArray
-        monsterImg.animationDuration = 0.8
-        monsterImg.animationRepeatCount = 0
-        monsterImg.startAnimating()
+        self.penalty1Img.alpha = self.DIM_ALPHA
+        self.penalty2Img.alpha = self.DIM_ALPHA
+        self.penalty3Img.alpha = self.DIM_ALPHA
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
+        
+        self.startTimer()
+    }
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
         
     }
-
-
+    
+    func startTimer() {
+        if self.timer != nil {
+            self.timer.invalidate()
+        }
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+        
+    }
+    
+    func changeGameState() {
+        
+        self.penalties++
+        
+        if self.penalties == 1 {
+            self.penalty1Img.alpha = self.OPAQUE
+            self.penalty2Img.alpha = self.DIM_ALPHA
+        } else if self.penalties == 2 {
+            self.penalty2Img.alpha = self.OPAQUE
+            self.penalty3Img.alpha = self.DIM_ALPHA
+        } else if self.penalties == 3 {
+            self.penalty3Img.alpha = self.OPAQUE
+        } else {
+            self.penalty1Img.alpha = self.DIM_ALPHA
+            self.penalty2Img.alpha = self.DIM_ALPHA
+            self.penalty3Img.alpha = self.DIM_ALPHA
+        }
+        
+        if self.penalties >= self.MAX_PENALTY {
+            self.gameOver()
+        }
+    }
+    
+    func gameOver() {
+        self.timer.invalidate()
+        self.monsterImg.playDeathAnimation()
+    }
 
 }
 
